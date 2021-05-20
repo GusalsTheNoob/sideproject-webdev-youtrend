f94832098e279f469e386f4edbaa66eebb29c699
GusalsTheNoob <harrygusals@snu.ac.kr>
2021-05-20 12:40:43 +0900

    Add basic middlewares to app

--- src/server.js
+++ src/server.js
@@ -1,5 +1,24 @@
+import MongoStore from "connect-mongo";
 import express from "express";
+import flash from "express-flash";
+import morgan from "morgan";
 
 const app = express();
+const logger = morgan("dev");
+
+app.set("view engine", "pug");
+app.set("views", process.cwd() + "/src/views");
+app.use(logger);
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(
+    session({
+        secret: process.env.COOKIE_SECRET, // need configuration
+        resave: false,
+        saveUninitialized: false,
+        store: MongoStore.create({ mongoUrl: process.env.DB_URL }),
+    })
+);
+app.use(flash());
 
 export default app;
